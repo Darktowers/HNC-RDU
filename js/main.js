079589e8674d5fb016e23d0c462ec7dd2328eb5b
Cristian Andres Arrieta <caarrieta40@misena.edu.co>
2015-05-19 13:13:24 -0500

    Optimizando 1.0

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 
 $(document).ready(function () {
+
+
+
   $('.Menu-link').click(function(e) {
             e.preventDefault();
             pag= $(this).attr("href");
@@ -11,3 +14,41 @@ $(window).load(function () {
   // Una vez se cargue al completo la página desaparecerá el div "cargando"
   $('#preloader').hide();
 });
+
+ $(document).ready(function() {
+ 
+          $("#owl-example-1").owlCarousel({
+            autoplay:true,
+            autoplayTimeout:3000,
+            items : 1,
+            loop:true,
+            nav: false,
+            center:true,
+            dots:false,
+            itemsDesktop : [1199,1],
+            itemsDesktopSmall : [979,1],
+            itemsTablet: [600,1], //2 items between 600 and 0
+            navigation : false,
+            pagination : false,
+            slideSpeed : 2200
+          });
+        $("#owl-example-2").owlCarousel({
+            autoplay:true,
+            items : 1,
+            autoplayTimeout:3000,
+            loop:true,
+            nav: false,
+            center:true,
+            dots:false,
+            itemsDesktop : [1199,2],
+            itemsDesktopSmall : [979,1],
+            itemsTablet: [600,1], //2 items between 600 and 0
+            navigation : false,
+            pagination : false,
+            slideSpeed : 50000,
+        
+          });
+
+         
+        });
+      
