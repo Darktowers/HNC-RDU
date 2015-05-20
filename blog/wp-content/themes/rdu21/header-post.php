88f31dc1e68e99319924575baea8a2a5eeed9cf8
Cristian Andres Arrieta <caarrieta40@misena.edu.co>
2015-05-20 17:03:20 -0500

    ajustando colores marca

--- blog/wp-content/themes/rdu21/header-post.php
+++ blog/wp-content/themes/rdu21/header-post.php
@@ -3,6 +3,7 @@
 <head>
 	<meta charset="UTF-8">
 	<title><?php the_title(); ?></title>
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
 	<link rel="stylesheet" href="<?php bloginfo(stylesheet_url)?>">
     <link href='http://fonts.googleapis.com/css?family=Lato:100italic,100,300italic,300,400,700,400italic' rel='stylesheet' type='text/css'> 
     <link href="https://fonts.googleapis.com/css?family=Crimson+Text:400,700,400italic,700italic" rel="stylesheet" type="text/css">
